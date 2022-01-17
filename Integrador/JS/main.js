f6586abddceec9b759202e55d46eeb5642364ca1
santtiq <santiquinteros@frba.utn.edu.ar>
2022-01-17 03:48:51 -0300

    New faces fot the cards, buttons working for all resolution and more little things

--- Integrador/JS/main.js
+++ Integrador/JS/main.js
@@ -1,20 +1,45 @@
-const articulos = document.getElementById("containerCards");
 
-function renderProducts(objetos, etiquetaCardHTML){
+//RENDERING PRODUCTS OF THE SHOP
+function renderProducts(arrayProducts, etiquetaCardHTML){
+    
     etiquetaCardHTML.innerHTML="";
-    for(products of objetos){
+    for(let product of arrayProducts){
         etiquetaCardHTML.innerHTML+=`
-        <div class="product" data-aos="fade-up">
-            <img src="${products.img}" alt="">
-            <div class="card-description">
-                <span class="card-tittle">${products.tittle}</span>
-                <span class="card-price">$ ${products.price}</span>
-                <span><b>3</b> cuotas sin interés de </span>
-                <button>Agregar al carrito</button>
-            </div>
-        </div>
+            <article class="product">
+                <div class="img-container">
+                    <img src="${product.img}" alt="product" class="product-img">
+                    <button class="bag-btn" data-id="1">
+                        <i class="fas fa-shopping-cart"></i>
+                        añadir al carrito
+                    </button>
+                </div>
+                <h3>${product.tittle}</h3>
+                <p>$ ${product.price}</p>
+                <p> <b>3</b> cuotas sin interés de </p>
+            </article>
         `
     }
 }
+renderProducts(products, newCard);
 
-renderProducts(products, articulos);
+
+// FUNCTIONS OF CART AND MENU-MOBILE
+buttonShowCart.onclick = function(){
+    document.getElementById('cartV').style.visibility = 'visible'
+    document.getElementById('cartT').style.transform = 'translateX(0%)'
+}
+
+buttonHideCart.onclick = function(){
+    document.getElementById('cartV').style.visibility = 'hidden'
+    document.getElementById('cartT').style.transform = 'translateX(100%)'
+}
+
+buttonShowMenu.onclick = function(){
+    document.getElementById("navLinks").style.display = "flex";
+    document.getElementById("navLinks").style.right = "0";   
+}
+
+buttonHideMenu.onclick = function(){
+    document.getElementById("navLinks").style.right = "-200px";
+    document.getElementById("navLinks").style.display = "none";
+}
---
+++ Integrador/JS/variables.js
@@ -0,0 +1,10 @@
+// OPEN AND CLOSE CART BUTTONS
+let buttonShowCart = document.getElementById('cart-btn');
+let buttonHideCart = document.getElementById('close-cart');
+
+//OPEN AND CLOSE MENU BUTTONS
+let buttonShowMenu = document.getElementById('show-menu');
+let buttonHideMenu = document.getElementById('hide-menu');
+
+//GETTING THE CONTAINER CARDS OF SHOP
+const newCard = document.getElementById("containerCards");
